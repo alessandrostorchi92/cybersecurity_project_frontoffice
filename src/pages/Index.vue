2966fd4a5a3e004caed6e5e1f447ac64f8df9c68
stefanopriolo <stefanojpriolo@gmail.com>
2023-11-06 20:19:26 +0100

    cards, show and api connection

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,43 +1,76 @@
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
-    components: {
+  data() {
+    return {
+      profiles: [],
+    };
+  },
+  methods: {
+    fetchDataProfile() {
+      axios
+        .get("http://127.0.0.1:8000/api/profile")
+        .then((response) => {
+          console.log(response.data);
+          this.profiles = response.data.data;
+        })
+        .catch((error) => {
+          console.error("Error fetching profile data:", error);
+        });
     },
-
-    data() {
-        return {
-            users:[]
-        };
+    getImageUrl(photo) {
+      return `http://127.0.0.1:8000/storage/${photo}`;
     },
-    methods: {
-        fetchData(){
-            axios
-            .get('http://127.0.0.1:8000/api/users')
-            .then((response)=>{
-                this.users = response.data.data;
-            })
+  },
 
-        }
-
-    },
-
-    mounted() {
-        this.fetchData()
-    },
+  mounted() {
+    this.fetchDataProfile();
+  },
 };
 </script>
 
-
 <template>
-    <div class="container">
-        <h1>Index</h1>
+  <div class="container">
+    <h1>Index</h1>
+
+    <!-- Visualizza le card degli utenti -->
+    <div class="row gap-5">
+      <div
+        class="card"
+        v-for="user in profiles"
+        :key="user.id"
+        style="width: 18rem"
+      >
+        <img
+          :src="getImageUrl(user.profile.photo)"
+          class="card-img-top"
+          alt="Immagine Professionista"
+        />
+        <div class="card-body">
+          <h5 class="card-title">{{ user.name }} {{ user.surname }}</h5>
+          <div v-if="user.profile">
+            <div>
+              <p>Location: {{ user.profile.location }}</p>
+              <p>Skills: {{ user.profile.skills }}</p>
+              <p>Description: {{ user.profile.description }}</p>
+              <p>
+                Specialization:
+                {{
+                  user.specializations
+                    .map((specialization) => specialization.name)
+                    .join(", ")
+                }}
+              </p>
+              <router-link :to="{ name: 'show', params: { id: user.id } }"
+                >Dettagli</router-link
+              >
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-
-<ul v-for="user in users">
-    <li><router-link :to="{name:'show', params: {id: user.id}}">{{ user.name}}</router-link></li>
-</ul>
-
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
--- src/router.js
+++ src/router.js
@@ -5,26 +5,27 @@ import IndexPage from "./pages/Index.vue";
 import ShowPage from "./pages/Show.vue";
 
 const routes = [
-    {
-        path: "/",
-        name: "home",
-        component: HomePage
-    },
-    {
-        path: "/index",
-        name: "index",
-        component: IndexPage
-    },
-    {
-        path: "/show/:id",
-        name: "show",
-        component: ShowPage
-    }
+  {
+    path: "/",
+    name: "home",
+    component: HomePage,
+  },
+  {
+    path: "/index",
+    name: "index",
+    component: IndexPage,
+  },
+  {
+    path: "/show/:id",
+    name: "show",
+    component: ShowPage,
+    props: true,
+  },
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes
+  history: createWebHistory(),
+  routes,
 });
 
 export { router };
