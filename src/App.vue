7bb4364cd5f204a7791416932017e920238d1b3f
amani esseili <esseiliamani@hotmail.com>
2023-11-08 15:26:33 +0100

    redesigned homeP with categories and card layout

--- src/App.vue
+++ src/App.vue
@@ -24,16 +24,16 @@ export default {
 
 <template>
   <header>
-    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-      <div class="container">
-        <a class="navbar-brand d-flex align-items-center" href="#">
-          <div>LOGO</div>
-        </a>
-        <ul class="navbar-nav me-auto">
-          <li class="nav-item"><router-link class="nav-link" to="/">Home</router-link></li>
-        </ul>
+    <div class="navigation-menu">
+      <img src="public/Cyber-Security-PNG-File.png" class="logo" alt="Logo" />
+  
+      <div class="menu-items">
+        <router-link class="me-5" to="/">Home</router-link>
+        <router-link class="me-5" to="/index">Index</router-link>
+        <a href="http://127.0.0.1:8000/login">Login </a>
       </div>
-    </nav>
+    </div>
+
   </header>
 
   <main>
@@ -43,4 +43,33 @@ export default {
   <footer></footer>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+
+
+/* Stili specifici per il tuo menu di navigazione */
+.navigation-menu {
+  display: flex;
+  align-items: center;
+  //background: rgba(0, 0, 0, 0.3);
+  padding:1rem;
+  margin-left: 2rem;
+  margin-right: 2rem;
+}
+
+.logo {
+  width: 10rem; /* Larghezza del logo */
+  margin-right: 20px; /* Spaziatura a destra del logo */
+}
+
+.menu-items {
+  flex-grow: 1; /* Flessibilità per far crescere lo spazio disponibile */
+  display: flex;
+  justify-content: flex-end;
+}
+
+.menu-items a {
+  color: #fff; /* Colore del testo delle voci del menu */
+  text-decoration: none;
+  margin-left: 20px; /* Spaziatura tra le voci del menu */
+}
+</style>
