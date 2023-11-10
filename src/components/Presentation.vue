152f6c5481317efbb28fa73a08e7fd117e0e3e17
amani esseili <esseiliamani@hotmail.com>
2023-11-10 17:10:45 +0100

    Added the Index slide 2 the presentation component

--- src/components/Presentation.vue
+++ src/components/Presentation.vue
@@ -13,119 +13,150 @@ export default {
       this.currentSlide -= 1;
     },
   },
-  
+
 }
 </script>
 <template>
-<div class="container-fluid">
-  <div class="row ">
-    <div class="col-lg-3 col-12 presentation-left  ">
-      <img class="mt-5" src="public/cyber-security-boolean-logo.png" style="width: 23rem;" alt="">
+  <div class="container-fluid">
+    <div class="row ">
+      <div class="col-lg-3 col-12 presentation-left  ">
+        <img class="mt-5" src="public/cyber-security-boolean-logo.png" style="width: 23rem;" alt="">
 
         <div class="col-md-12 p-4 mar ">
           <p class="description flex-grow-1"><strong class="info">Maestro :</strong> Florian Leica</p>
           <p class="description"><strong class="info">Coordinatrore :</strong> Elisa Dahò</p>
           <p class="description"><strong class="info">Project Manager :</strong> Donato Riccio & Jacopo Damiani</p>
-          <p class="description"><strong class="info">Studenti :</strong> alessandro Storchi, Amani Esseili, Dario Fusco, Ludovico Pellecchia e Stefano Priolo</p>
-
-
+          <p class="description"><strong class="info">Studenti :</strong> alessandro Storchi, Amani Esseili, Dario Fusco,
+            Ludovico Pellecchia e Stefano Priolo</p>
         </div>
+      </div>
 
 
-    </div>
+      <div class="col-lg-9 col-12 presentation-right ">
+        <div class="row justify-content-center">
+          <div class="col-md-10">
+            <div class="presentaion-bg">
+              <!-- ---------slide-1----------- -->
 
+              <div v-if="currentSlide === 1" class="slide first-slide-bg col-md-6" id="slide-1">
 
-    <div class="col-lg-9 col-12 presentation-right ">
-            <div class="row justify-content-center">
-                <div class="col-md-10">
-                  <div class="presentaion-bg">
-                    <!-- ---------slide-1----------- -->
-                    
-                    <div v-if="currentSlide === 1" class="slide first-slide-bg col-md-6" id="slide-1">
-                      
-                      <h1 class="title slide1-title">Benvenuti nel Futuro della Sicurezza</h1>
-                      <p class="description">Benvenuti alla presentazione della nostra app di sicurezza informatica. Siamo entusiasti di mostrarvi come abbiamo progettato un'app robusta e avanzata per proteggere i vostri dati sensibili, offrendo funzionalità di ricerca e analisi avanzate nel contesto della cyber security.</p>
-                      <button class="btn" @click="nextSlide">Prossima Slide</button>
+                <h1 class="title slide1-title">Benvenuti nel Futuro della Sicurezza</h1>
+                <p class="description">Benvenuti alla presentazione della nostra app di sicurezza informatica. Siamo
+                  entusiasti di mostrarvi come abbiamo progettato un'app robusta e avanzata per proteggere i vostri dati
+                  sensibili, offrendo funzionalità di ricerca e analisi avanzate nel contesto della cyber security.</p>
+                <button class="btn" @click="nextSlide">Prossima Slide</button>
 
-                    </div>
-                    <!-- ---------slide-2----------- -->
-                    <div v-else-if="currentSlide === 2" class="slide" id="slide-2">
-                      <h2>Indice: Ricerca e Analisi</h2>
-                      <p>Concept e progetto</p>
-                      <button class="btn" @click="prevSlide">Indietro</button>
-                      <button class="btn" @click="nextSlide">Prossima Slide</button>
-                    </div>
-                    <!-- ---------slide-3----------- -->
-                    
+              </div>
+              <!-- ---------slide-2----------- -->
+              <div v-else-if="currentSlide === 2" class="slide col-10" id="slide-2">
+
+                <h2 class="title">Che cos'è CyberSecurity ? </h2>
+                <p class="description">CyberSecure e un app che fa la differenza nell’era digitale. La crescente minnaccia
+                  delle violazioni delle sicurezza digitale richiede soluzioni affidabili.
+                  Con nostra app l’utente puo trovare esperti di sicurezza informatica altamente qualificati per
+                  proteggere i loro dati digitali e loro sistemi.
+                </p>
+                <h3 class="text-start title ms-3">Indice</h3>
+                <div class="d-flex">
+                  <div class="col-4 p-1 index-bg">
+                    <h6 class="info">Analisi e Ricerca</h6>
+                    <p class="description">Ricerca per comprendere il contesto dell’app .Qual è il suo scopo? Chi sono gli utenti target? Quali sono le esigenze degli utenti? 
+                    </p>
+
                   </div>
-                    
-
-
+                  <div class="col-4 p-1 index-bg">
+                    <h6 class="info">Design</h6>
+                    <p class="description">Un design pulito e organizzato per trasmettere un senso di sicurezza, professionalita e tecnologia.
+                      </p>
+                  </div>
+                  <div class="col-4 p-1 index-bg">
+                    <h6 class="info">Progetto</h6>
+                    <p class="description">Utilizzo di Vue per il Frontend e Lravel per il Backend.</p>
+                  </div>
 
                 </div>
+
+                <button class="btn me-4" @click="prevSlide">Indietro</button>
+                <button class="btn" @click="nextSlide">Prossima Slide</button>
+
+              </div>
+              <!-- ---------slide-3----------- -->
+              <div v-else-if="currentSlide === 3" class="slide" id="slide-3">
+                <h2>Analisi e Ricerca </h2>
+                <p>Analisi e Ricerca</p>
+                <button class="btn me-4" @click="prevSlide">Indietro</button>
+                <button class="btn" @click="nextSlide">Prossima Slide</button>
+              </div>
+
             </div>
+
+
+
+
+          </div>
+        </div>
+      </div>
     </div>
-</div>
-</div>
+  </div>
 
-<!-- ------------- -->
-
-
-
+  <!-- ------------- -->
 </template>
 <style lang="scss" scoped>
 /* stili per full body background */
 .presentation-left {
-	background-color:#202426;
-	color: #fff;
-	height: 100vh;
+  background-color: #202426;
+  color: #fff;
+  height: 100vh;
 }
-.presentation-right{
-  background-image: url(/presentation-bg.jpg) ;
+
+.presentation-right {
+  background-image: url(/presentation-bg.jpg);
   background-attachment: fixed; //fissa il bg-img per evitare lo scrolling
-	background-repeat: no-repeat; 
+  background-repeat: no-repeat;
   background-size: contain;
   background-position: right;
   height: 100vh;
 }
 
-.mar{
+.mar {
   margin-top: 32rem;
 }
-.info{
+
+.info {
   font-size: 1rem;
-  color:  #1EBFCB;
+  color: #1EBFCB;
   font-weight: bold
+}
+
+.title {
+  font-size: 3rem;
+  color: #1EBFCB;
 
 }
 
-.title{
-	font-size: 2rem;
-	color:  #1EBFCB;
-
-}
-.description{
-	color: #b0b1b2;
+.description {
+  color: #b0b1b2;
   font-size: 1rem;
 }
+
 .btn {
-	cursor: pointer;
-	background-color: rgb(37, 37, 37);
-	color: #1EBFCB;
-	padding: 10px 20px;
-	font-size: 1.2rem;
-	text-decoration: none;
-	border-radius: 5px;
-	transition: background-color 0.3s;
-	border-radius: 15px;
-	text-align: center;
-	padding: 8px 10px;
-	width: 15rem;
+  cursor: pointer;
+  background-color: rgb(37, 37, 37);
+  color: #1EBFCB;
+  padding: 10px 20px;
+  font-size: 1.2rem;
+  text-decoration: none;
+  border-radius: 5px;
+  transition: background-color 0.3s;
+  border-radius: 15px;
+  text-align: center;
+  padding: 8px 10px;
+  width: 15rem;
 
 }
 
 .btn:hover {
-	background-color: #6d7074;
+  background-color: #6d7074;
 }
 
 /*.first-slide-bg {
@@ -145,17 +176,45 @@ export default {
   padding: 20px;
 }
 
+/*--------------------------*/
 /* stili per gli stili specifici di ciascuna slide  */
 /* stili specifici per la prima slide-1 */
 #slide-1 {
-  
-  .slide1-title{
+
+  .slide1-title {
     margin-top: 20rem;
     //margin-right: 12rem;
   }
 }
-/*--------------------------------*/
 
+/*------------------------------------*/
+/* stili specifici per la prima slide-1 */
+#slide-2 {
 
+  .title {
+    margin-top: 6rem;
+    margin-bottom: 2rem;
+  }
 
-</style>
+  .description {
+    margin-bottom: 2rem;
+  }
+
+  .index-bg {
+    height: 10rem;
+    background-color: rgba(51, 51, 51, 0.5);
+    border-left: 10px solid rgb(31, 192, 204, );
+    ;
+    border-radius: 10px;
+    margin: 1rem;
+  }
+  .index-bg:hover {
+    background-color:  rgba(51, 51, 51, 0.9);
+  }
+
+  .btn {
+    margin-top: 3rem;
+  }
+}
+
+/*--------------------------------*/</style>
